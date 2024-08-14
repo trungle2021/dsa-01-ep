27a60bcf9c6dc8deb9b50ca0aeded835d5075502
Trung Le <84488011+trungle2021@users.noreply.github.com>
2024-08-14 15:56:44 +0700

    .

--- dsa01/day01/PascalTriangle.java
+++ dsa01/day01/PascalTriangle.java
@@ -1,4 +1,40 @@
 package day01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangle {
+    public static void main(String[] args) {
+        int numRows = 5;
+
+        System.out.println(generate(numRows));
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> firstRow = new ArrayList<>();
+        List<Integer> secondRow = new ArrayList<>();
+        firstRow.add(1);
+        secondRow.add(1);
+        secondRow.add(1);
+
+
+        for (int i = 2; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j = 1; j <= i - 1; j++){
+                row.add(1); 
+            }
+        }
+
+        result.add(firstRow);
+
+
+
+
+
+        return result;
+    }
+
+
 }
--- dsa01/day01/RemoveElement.java
+++ dsa01/day01/RemoveElement.java
@@ -4,15 +4,32 @@ public class RemoveElement {
     public static void main(String[] args) {
         int[] nums = {3, 2, 2, 3};
         int val = 3;
-        System.out.println(removeElement(nums, val));
+        System.out.println(removeElement2(nums, val));
     }
 
-    public static int removeElement(int[] nums, int val){
+    public static int removeElement2(int[] nums, int val){
         int n = nums == null ? 0 : nums.length;
-        if(n > 100) return 0;
-        for (int i = 0; i < n; i++){
-//            if(nums[i])
-        }
-//        return k;
+        int i = 0;
+       while(i < n){
+           if(nums[i] != val){
+               nums[i] = nums[n-1];
+               n--;
+           }else{
+               i++;
+           }
+       }
+       return n;
     }
+
+//    public static int removeElement(int[] nums, int val){
+//        int n = nums == null ? 0 : nums.length;
+//        int j = 0;
+//       for(int i = 0; i < n; i++){
+//           if(nums[i] != val){
+//               nums[j] = nums[i];
+//               j++;
+//           }
+//       }
+//        return j;
+//    }
 }
