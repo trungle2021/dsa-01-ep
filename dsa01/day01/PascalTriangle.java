bf7d30ef7e8cdf875e51290c2af31f92954ced34
Trung Le <84488011+trungle2021@users.noreply.github.com>
2024-08-14 16:14:18 +0700

    .

--- dsa01/day01/PascalTriangle.java
+++ dsa01/day01/PascalTriangle.java
@@ -18,20 +18,19 @@ public class PascalTriangle {
         firstRow.add(1);
         secondRow.add(1);
         secondRow.add(1);
-
+        result.add(firstRow);
+        result.add(secondRow);
 
         for (int i = 2; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
+            row.add(1);
             for(int j = 1; j <= i - 1; j++){
-                row.add(1); 
+                Integer value1 = result.get(i-1).get(j-1);
+                Integer value2 = result.get(i-1).get(j);
+                row.add(value1 + value2);
             }
+            row.add(1);
         }
-
-        result.add(firstRow);
-
-
-
-
 
         return result;
     }
