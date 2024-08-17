4f6d5981c25d3d3294fc9eb1ba90623dcc89f235
trungle2021 <leanhtrung97@gmail.com>
2024-08-17 09:47:48 +0700

    .

--- dsa01/day01/CanPlaceFlowers.java
+++ dsa01/day01/CanPlaceFlowers.java
@@ -27,49 +27,50 @@ package day01;
  */
 public class CanPlaceFlowers {
     public static void main(String[] args) {
-        int n = 2;
-//        int[] flowerbed = {1,0,0,0,0,0,1};
+        int n = 0;
 //        int[] flowerbed = {1,0,0,0,0,0,1};
 //        int[] flowerbed = {1,0,0,0,0,0,0,1};
 //        int[] flowerbed = {1,0,0,0,1,0,0};
-        int[] flowerbed = {0,0,1,0,1};
+//        int[] flowerbed = {0,0,1,0,1};
 //        int[] flowerbed = {1,0,1,0,1,0,1};
+//        int[] flowerbed = {1,0,0,1,1,0,0};
+//        int[] flowerbed = {0,0,1,0,0};
+//        int[] flowerbed = {0,0};
+        int[] flowerbed = {1};
+//        int[] flowerbed = {0,0,0,0};
         System.out.println(canPlaceFlowers(flowerbed, n));
     }
 
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
         int length = flowerbed == null ? 0 : flowerbed.length;
-        int result = 0;
-        int emptyPlots = 0;
 
-        if(length == 1 && flowerbed[0] == 0){
+        if(n==0){
             return true;
         }
 
-        if(flowerbed[0] == 0 && flowerbed[1] == 0) {
-            result++;
+        if(length == 1 && flowerbed[0] == 0) return true;
+        if(length == 2 && (flowerbed[0] == 1 || flowerbed[1] == 1)) return false;
+        if(length == 2 && n <= 1 && (flowerbed[0] == 0 && flowerbed[1] == 0)) return true;
+        if(flowerbed[0] == 0 && flowerbed[1] == 0){
+            flowerbed[0] = 1;
+            n--;
+        }
+        if (flowerbed[length -1] == 0 && flowerbed[length - 2] == 0){
+            flowerbed[length-1] = 1;
+            n--;
         }
 
-        if(flowerbed[length -1] == 0 && flowerbed[length - 2] == 0){
-            result++;
-        }
-
-        for (int i = 0; i < length -1; i++) {
+        for (int i = 1; i < length -1; i++) {
+            if(n<=0) return true;
+            int before = flowerbed[i-1];
             int current = flowerbed[i];
             int next = flowerbed[i + 1];
 
-            if(current == 0 && next == 0 || current == 0 && next == 1){
-                emptyPlots += 1;
-                if(emptyPlots - n >= 2) result++;
-            }else {
-                emptyPlots = 0;
+            if(current == 0 && before == 0 && next == 0){
+                flowerbed[i] = 1;
+                n--;
             }
         }
-
-        if(result >= n){
-            return true;
-        }
-
         return false;
     }
 }
